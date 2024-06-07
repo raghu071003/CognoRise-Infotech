c978721d1d03b62e220e70ac616121e126d4c77a
raghu071003 <131893227+raghu071003@users.noreply.github.com>
2024-06-07 11:06:12 +0530

    Done
    Done

--- Calculator/script.js
+++ Calculator/script.js
@@ -2,13 +2,7 @@ const switchbtn = document.getElementById("switch");
 const display = document.getElementById("display");
 const input = document.querySelectorAll("input");
 let value = "";
-switchbtn.addEventListener("click", () => {
-    if(switchbtn.checked == true){
-        document.body.setAttribute("data-theme", "dark");
-    }else{
-        document.body.setAttribute("data-theme", "");
-    }
-});
+
 
 input.forEach((e) => {
     e.addEventListener("click", (event) => {
